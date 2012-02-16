714e522ec9d06d91450c3ba012b6cb19190f894a
Matthew Nielsen <xunker@pyxidis.org>
2012-02-15 18:36:13 -0700

    Revert "Better loading, better layout, better rendering!"
    This reverts commit d51ccdd0fd183e9ee5ff7519297aab0a1f0201d1.

--- assets/www/base.js
+++ assets/www/base.js
@@ -312,19 +312,6 @@ function handleMenuButton () {
 
 }
 
-function getParameterByName(name)
-{
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regexS = "[\\?&]" + name + "=([^&#]*)";
-  var regex = new RegExp(regexS);
-  var results = regex.exec(window.location.search);
-  if(results == null)
-    return "";
-  else
-    return decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-
 // replaced onBodyLoad
 function assimilate () {
   document.addEventListener("deviceready", onDeviceReady, false);
